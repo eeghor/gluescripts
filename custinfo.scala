0935db4b8f00233e6642cc92eae33b233ed7f156
Igor Korostil <eeghor@gmail.com>
2018-05-25 17:30:43 +1000

    in progress

--- custinfo.scala
+++ custinfo.scala
@@ -11,9 +11,17 @@ val df = spark.read.option("inferSchema", "true").option("header", "true").csv("
 /* filter out customers who aren't on the Ticketek customer list (list number 2) and 
 those who have no customer id (we expect that all customer do)
 */
-df.filter(df("CustomerID").isNotNull)
-	.filter(df("CustomerListId") === 2)
-	.filter(df("EmailAddress").contains("@"))
-	.filter(!df("EmailAddress").contains("ticketek")).count()
-// filter out invalid postcodes
-//df.withColumn("cleanPostcode", regexp_extract(df("Postcode"),"\b(([2-8]\\d{3})|([8-9]\\d{2}))\b",0)).collect()
+df.filter($"CustomerID".isNotNull)
+	.filter($"CustomerListId" === 2)
+//	.select(df.columns.map(c => lower(col(c)).alias(c)): _*)
+	.filter(!df("EmailAddress").contains("ticketek"))
+	.withColumn("EmailAddress", when($"EmailAddress".contains("@"), $"EmailAddress").otherwise(""))
+	.withColumn("Postcode", regexp_extract($"Postcode","\b(([2-8]\\d{3})|([8-9]\\d{2}))\b",0))
+	.withColumn("Salutation", lower(regexp_replace($"Salutation","[^A-Za-z]","")))
+	.withColumn("Salutation", when($"Salutation".isin(List("mr","mrs","miss","mister","dr"):_*), $"Salutation").otherwise(""))
+	.withColumn("DateOfBirth", when(year($"DateOfBirth") < 1918, lit(null)).otherwise($"DateOfBirth"))
+	.select("CustomerID", "Salutation", "FirstName", "LastName", "DateOfBirth", 
+				"CreatedDate", "ModifiedDate", "EmailAddress","City","Postcode",
+					"CountryName","MobilePhone", "HomePhone", "WorkPhone")
+	.repartition(1).write.option("header","true").mode("overwrite").option("compression", "gzip").csv("out")
+
