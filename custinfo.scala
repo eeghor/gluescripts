109f53b1040ef395238d463db53b49a49087c3d8
Igor Korostil <eeghor@gmail.com>
2018-05-25 23:57:25 +1000

    going well

--- custinfo.scala
+++ custinfo.scala
@@ -11,17 +11,25 @@ val df = spark.read.option("inferSchema", "true").option("header", "true").csv("
 /* filter out customers who aren't on the Ticketek customer list (list number 2) and 
 those who have no customer id (we expect that all customer do)
 */
-df.filter($"CustomerID".isNotNull)
-	.filter($"CustomerListId" === 2)
+df.filter(df("CustomerID").isNotNull)
+	.filter(df("CustomerListId") === 2)
 //	.select(df.columns.map(c => lower(col(c)).alias(c)): _*)
 	.filter(!df("EmailAddress").contains("ticketek"))
-	.withColumn("EmailAddress", when($"EmailAddress".contains("@"), $"EmailAddress").otherwise(""))
-	.withColumn("Postcode", regexp_extract($"Postcode","\b(([2-8]\\d{3})|([8-9]\\d{2}))\b",0))
-	.withColumn("Salutation", lower(regexp_replace($"Salutation","[^A-Za-z]","")))
-	.withColumn("Salutation", when($"Salutation".isin(List("mr","mrs","miss","mister","dr"):_*), $"Salutation").otherwise(""))
-	.withColumn("DateOfBirth", when(year($"DateOfBirth") < 1918, lit(null)).otherwise($"DateOfBirth"))
+	.withColumn("EmailAddress", when(df("EmailAddress").contains("@"), lower(df("EmailAddress"))).otherwise(""))
+	.withColumn("Postcode", regexp_extract(df("Postcode"),"\\b(([2-8]\\d{3})|([8-9]\\d{2}))\\b",0))
+	.withColumn("Salutation", when(lower(regexp_replace(df("Salutation"),"[^A-Za-z]",""))
+		.isin(List("mr","ms","mrs","dr","mister","miss"):_*), lower(df("Salutation")))
+		.otherwise(""))
+	.withColumn("DateOfBirth", when(year(df("DateOfBirth")) < 1918, lit(null)).otherwise(df("DateOfBirth")))
+	.withColumn("FirstName", ltrim(lower(regexp_replace(df("FirstName"), "[-]"," "))))
+	.withColumn("LastName", ltrim(lower(regexp_replace(regexp_replace(df("LastName"),"['`]",""),"[-]"," "))))
+	.withColumn("MobilePhone", regexp_extract(regexp_replace(df("MobilePhone"),"\\s",""),"(\\+*(?:61)*|0*)(4\\d{8})",2))
+	.withColumn("State", lower(df("State")))
+	.withColumn("City", lower(df("City")))
+	.withColumn("CountryName", lower(df("CountryName")))
 	.select("CustomerID", "Salutation", "FirstName", "LastName", "DateOfBirth", 
-				"CreatedDate", "ModifiedDate", "EmailAddress","City","Postcode",
+				"CreatedDate", "ModifiedDate", "EmailAddress","State", "City","Postcode",
 					"CountryName","MobilePhone", "HomePhone", "WorkPhone")
-	.repartition(1).write.option("header","true").mode("overwrite").option("compression", "gzip").csv("out")
+	.repartition(1)
+	.write.option("header","true").mode("overwrite").option("compression", "gzip").csv("out")
 
