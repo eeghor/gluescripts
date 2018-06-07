238b81f0af47cf37ecea7acd01cac58fabbdb135
eeghor <eeghor@gmail.com>
2018-06-07 23:45:38 +1000

    gender

--- custinfo.py
+++ custinfo.py
@@ -2,6 +2,159 @@ from pyspark.sql.types import *
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import re
+import json
+
+# s3 = boto3.client('s3')
+
+# for dfl in ['data_names_.json', 'data_salutations_.json', 'data_hypocorisms_.json', 'data_grammgender_.json']:
+# 	s3.download_fileobj(Bucket='tega-uploads', Key='Igor/temp/gender_data/{}'.format(dfl), Fileobj=open(dfl,'wb'))
+
+name_db = json.load(open('data_names_.json'))
+title_db = json.load(open('data_salutations_.json'))
+hypoc_db = json.load(open('data_hypocorisms_.json'))
+grammg_db = json.load(open('data_grammgender_.json'))
+
+# name_db, title_db, hypoc_db, grammg_db = [json.load(open(f,'r')) for f in [os.path.join(os.path.dirname(__file__),'data/data_names_.json'), 
+# 																 		   os.path.join(os.path.dirname(__file__),'data/data_salutations_.json'), 
+# 																 		   os.path.join(os.path.dirname(__file__),'data/data_hypocorisms_.json'),
+# 																 		   os.path.join(os.path.dirname(__file__),'data/data_grammgender_.json')]]
+
+# info = f'dictionaries: {len(name_db)} names, {len(hypoc_db)} hypocorisms, {len(grammg_db)} grammatical gender words'
+																 
+# def __init__(self, priority='name'):
+
+# 	 title_gender =  name_gender =  email_gender = likely_gender = None
+# 	 PRIORITY = priority
+
+
+
+def _longest_common(set_a, set_b):
+
+	"""
+	helper to find the longest common word for sets a and b
+	"""
+	_ = set_a & set_b
+	
+	return max(_, key=len) if _ else None
+
+
+def get_gender(title, name, email):
+
+	"""
+	detect gender by first name, title and email and then decide which one to pick as the likeliest gender
+	"""
+
+	# normalization
+	
+	title, name, email = [unidecode(_).strip().lower() if isinstance(_, str) else None 
+												for _ in [title, name, email]]  
+	
+	if title:
+
+		title = ''.join([c for c in title if c in ascii_lowercase])
+	
+	if name:
+		# name: only keep letters, white spaces (reduced to a SINGLE white space) and hyphens
+		name = ' '.join(''.join([c for c in name.lower() 
+									if (c in ascii_lowercase) or (c in [' ','-'])]).split()).strip()
+		if not (set(name) & set(ascii_lowercase)):
+			name = None
+
+	if email:
+		email = ''.join([c for c in  email.split('@')[0].lower().strip() if (c in ascii_lowercase) or (c in ['.','-','_'])])
+		
+		if not email:
+			 email = None
+
+	# gender from title
+
+	title_gender = None
+
+	for g in 'm f'.split():
+
+		if title and (title in  title_db['common'][g] +  title_db['uncommon'][g]):
+			 title_gender = g
+
+	# gender from name
+
+	name_gender = None
+
+	if not name:
+		 name_gender = None
+
+	else:
+
+		nameparts = {_ for w in  name.split() for _ in w.split('-')}
+
+		_ =  _longest_common(nameparts, set(name_db) | set(hypoc_db))
+
+		if not _:
+			pass
+		else:
+
+			if _ in  name_db:
+		 		name_gender =  name_db[_]
+			else:
+				# find what names corresp.to hypocorism and are in the name database
+				_ =  _longest_common(set( hypoc_db[_]), set( name_db))
+
+				if _ and ( name_db[_] != 'u'):
+			 		name_gender =  name_db[_]
+
+	if  name_gender not in 'm f'.split():
+		 name_gender = None
+	
+	# gender from email
+
+	email_gender = None
+
+	if email:
+	
+		# find any names in the email prefix; ONLY those separated by _, - or . count
+		emailparts = set(q.strip() for w in  email.split('_') 
+									for v in w.split('.') 
+										for q in v.split('-') if q.strip())
+
+		_ =  _longest_common(emailparts, set( name_db))
+
+		if _ and ( name_db[_] != 'u'):
+			email_gender =  name_db[_]
+		else:
+			_ =  _longest_common(emailparts, set( hypoc_db))
+
+			if _ and (_ in  name_db) and ( name_db[_]!= 'u'):
+		 		email_gender =  name_db[longest_hyp]
+		 	else:
+
+				# last resort: grammatical gender
+
+				_ =  _longest_common(emailparts, set( grammg_db))
+
+				if _:
+		 			email_gender =  grammg_db[_]
+
+
+	likely_gender = None
+
+	# the title based suggestion is available and the same as the name based one
+	if all([ title_gender,  name_gender,  title_gender ==  name_gender]):
+		likely_gender =  name_gender
+	# both are available but NOT the same
+	elif all([ name_gender,  title_gender]):
+		if  PRIORITY == 'name':
+			likely_gender =  name_gender
+		else:
+			likely_gender =  title_gender
+	# what is only one is available
+	elif all([ title_gender, not  name_gender]):
+		likely_gender =  title_gender
+	elif all([ name_gender, not  title_gender]):
+		likely_gender =  name_gender
+	# finally, if the email based is the only one we have, use it
+	elif  email_gender:
+		likely_gender =  email_gender
+
+	return likely_gender
 
 
 def getDomain(s):
@@ -85,6 +238,9 @@ def isUni(s):
 
 	return unis.get(p, "none")
 
+_longest_commonUDF = udf(_longest_common)
+
+get_genderUDF = udf(get_gender)
 
 isUniUDF = udf(isUni, StringType())
 
