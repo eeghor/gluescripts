578eb85e145eea81e932a17035dd49696958acad
eeghor <eeghor@gmail.com>
2018-06-08 18:18:29 +1000

    gender works locally

--- custinfo.py
+++ custinfo.py
@@ -2,160 +2,48 @@ from pyspark.sql.types import *
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import re
+import os
 import json
+import builtins
 
-# s3 = boto3.client('s3')
+# get useful files for gender detection
+name_db, title_db, hypoc_db, grammg_db = [json.load(open(f,'r')) for f in [os.path.join(os.path.dirname(__file__),'data/data_names_.json'), 
+																	 		   os.path.join(os.path.dirname(__file__),'data/data_salutations_.json'), 
+																	 		   os.path.join(os.path.dirname(__file__),'data/data_hypocorisms_.json'),
+																	 		   os.path.join(os.path.dirname(__file__),'data/data_grammgender_.json')]]
 
-# for dfl in ['data_names_.json', 'data_salutations_.json', 'data_hypocorisms_.json', 'data_grammgender_.json']:
-# 	s3.download_fileobj(Bucket='tega-uploads', Key='Igor/temp/gender_data/{}'.format(dfl), Fileobj=open(dfl,'wb'))
-
-name_db = json.load(open('data_names_.json'))
-title_db = json.load(open('data_salutations_.json'))
-hypoc_db = json.load(open('data_hypocorisms_.json'))
-grammg_db = json.load(open('data_grammgender_.json'))
-
-# name_db, title_db, hypoc_db, grammg_db = [json.load(open(f,'r')) for f in [os.path.join(os.path.dirname(__file__),'data/data_names_.json'), 
-# 																 		   os.path.join(os.path.dirname(__file__),'data/data_salutations_.json'), 
-# 																 		   os.path.join(os.path.dirname(__file__),'data/data_hypocorisms_.json'),
-# 																 		   os.path.join(os.path.dirname(__file__),'data/data_grammgender_.json')]]
-
-# info = f'dictionaries: {len(name_db)} names, {len(hypoc_db)} hypocorisms, {len(grammg_db)} grammatical gender words'
-																 
-# def __init__(self, priority='name'):
-
-# 	 title_gender =  name_gender =  email_gender = likely_gender = None
-# 	 PRIORITY = priority
-
-
-
-def _longest_common(set_a, set_b):
-
+def getGenderTitle(s):
 	"""
-	helper to find the longest common word for sets a and b
+	string s is presumably a title
 	"""
-	_ = set_a & set_b
-	
-	return max(_, key=len) if _ else None
-
-
-def get_gender(title, name, email):
-
-	"""
-	detect gender by first name, title and email and then decide which one to pick as the likeliest gender
-	"""
-
-	# normalization
-	
-	title, name, email = [unidecode(_).strip().lower() if isinstance(_, str) else None 
-												for _ in [title, name, email]]  
-	
-	if title:
-
-		title = ''.join([c for c in title if c in ascii_lowercase])
-	
-	if name:
-		# name: only keep letters, white spaces (reduced to a SINGLE white space) and hyphens
-		name = ' '.join(''.join([c for c in name.lower() 
-									if (c in ascii_lowercase) or (c in [' ','-'])]).split()).strip()
-		if not (set(name) & set(ascii_lowercase)):
-			name = None
-
-	if email:
-		email = ''.join([c for c in  email.split('@')[0].lower().strip() if (c in ascii_lowercase) or (c in ['.','-','_'])])
-		
-		if not email:
-			 email = None
-
-	# gender from title
-
-	title_gender = None
-
 	for g in 'm f'.split():
+		if s and (s.lower() in title_db['common'][g] + title_db['uncommon'][g]):
+				return g
 
-		if title and (title in  title_db['common'][g] +  title_db['uncommon'][g]):
-			 title_gender = g
+def getGenderName(s):
+	"""
+	s is a string possibly containing name
+	"""
+	if not s:
+		return None
 
-	# gender from name
+	# any name found straight away or hypocorisms? hypoc_db has hypocs as keys
+	nameparts_incommon = {_ for w in s.split() for _ in w.split('-')} & (set(name_db) | set(hypoc_db))
 
-	name_gender = None
+	if nameparts_incommon:
 
-	if not name:
-		 name_gender = None
+		name_cand = builtins.max(nameparts_incommon, key=len)
 
-	else:
-
-		nameparts = {_ for w in  name.split() for _ in w.split('-')}
-
-		_ =  _longest_commonUDF(nameparts, set(name_db) | set(hypoc_db))
-
-		if not _:
-			pass
+		# if candidate is name (and not hypoc)
+		if name_cand in name_db:
+			return name_db[name_cand]
 		else:
+			# find what names corresp.to hypocorism and are in the name database
+			unfolded_hypoc = set(hypoc_db[name_cand]) & set(name_db)
 
-			if _ in  name_db:
-		 		name_gender =  name_db[_]
-			else:
-				# find what names corresp.to hypocorism and are in the name database
-				_ =  _longest_commonUDF(set( hypoc_db[_]), set( name_db))
-
-				if _ and ( name_db[_] != 'u'):
-			 		name_gender =  name_db[_]
-
-	if  name_gender not in 'm f'.split():
-		 name_gender = None
-	
-	# gender from email
-
-	email_gender = None
-
-	if email:
-	
-		# find any names in the email prefix; ONLY those separated by _, - or . count
-		emailparts = set(q.strip() for w in  email.split('_') 
-									for v in w.split('.') 
-										for q in v.split('-') if q.strip())
-
-		_ =  _longest_commonUDF(emailparts, set( name_db))
-
-		if _ and ( name_db[_] != 'u'):
-			email_gender =  name_db[_]
-		else:
-			_ =  _longest_commonUDF(emailparts, set( hypoc_db))
-
-			if _ and (_ in  name_db) and ( name_db[_]!= 'u'):
-		 		email_gender =  name_db[longest_hyp]
-		 	else:
-
-				# last resort: grammatical gender
-
-				_ =  _longest_commonUDF(emailparts, set( grammg_db))
-
-				if _:
-		 			email_gender =  grammg_db[_]
-
-
-	likely_gender = None
-
-	# the title based suggestion is available and the same as the name based one
-	if all([ title_gender,  name_gender,  title_gender ==  name_gender]):
-		likely_gender =  name_gender
-	# both are available but NOT the same
-	elif all([ name_gender,  title_gender]):
-		if  PRIORITY == 'name':
-			likely_gender =  name_gender
-		else:
-			likely_gender =  title_gender
-	# what is only one is available
-	elif all([ title_gender, not  name_gender]):
-		likely_gender =  title_gender
-	elif all([ name_gender, not  title_gender]):
-		likely_gender =  name_gender
-	# finally, if the email based is the only one we have, use it
-	elif  email_gender:
-		likely_gender =  email_gender
-
-	return likely_gender
-
+			if unfolded_hypoc:
+				hyp_cand = builtins.max(unfolded_hypoc, key=len)
+				return name_db[hyp_cand]
 
 def getDomain(s):
 
@@ -238,9 +126,9 @@ def isUni(s):
 
 	return unis.get(p, "none")
 
-_longest_commonUDF = udf(_longest_common)
+getGenderTitleUDF = udf(getGenderTitle, StringType())
 
-get_genderUDF = udf(get_gender)
+getGenderNameUDF = udf(getGenderName, StringType())
 
 isUniUDF = udf(isUni, StringType())
 
@@ -258,25 +146,26 @@ df1 = df.filter(df.CustomerID.isNotNull()) \
 		.filter(df.CustomerListID == 2) \
 		.filter(~df.EmailAddress.contains("ticketek"))
 
-df2 = df1.withcolumn("emailaddress_", \
-					when(df1.emailaddress.contains("@"), lower(df1.emailaddress)).otherwise(""))
+df2 = df1.withColumn("EmailAddress_", \
+					when(df1.EmailAddress.contains("@"), lower(df1.EmailAddress)).otherwise(""))
 
-df3 = df2.withcolumn("university", isuniudf(df2.emailaddress_)) \
-		.withcolumn("isstudentorstaff", isstudentorstaffudf(df2.emailaddress_)) \
-		.withcolumn("postcode", regexp_extract(df2.postcode,"\\b(([2-8]\\d{3})|([8-9]\\d{2}))\\b",0)) \
-		.withColumn("salutation", when(lower(regexp_replace(df2.salutation,"[^A-Za-z]","")) \
-										.isin(["mr","ms","mrs","dr","mister","miss"]), lower(df.salutation)) \
+df3 = df2.withColumn("University", isUniUDF(df2.EmailAddress_)) \
+		.withColumn("isStudentOrStaff", isStudentOrStaffUDF(df2.EmailAddress_)) \
+		.withColumn("Postcode", regexp_extract(df2.Postcode,"\\b(([2-8]\\d{3})|([8-9]\\d{2}))\\b",0)) \
+		.withColumn("Salutation", when(lower(regexp_replace(df2.Salutation,"[^A-Za-z]","")) \
+										.isin(["mr","ms","mrs","dr","mister","miss"]), lower(df.Salutation)) \
 										.otherwise("")) \
-		.withColumn("dateofbirth", when(year(df2.dateofbirth) < 1918, lit(None)).otherwise(df2.dateofbirth)) \
-		.withColumn("firstname", ltrim(lower(regexp_replace(df2.firstname, "[-]"," ")))) \
-		.withColumn("lastname", ltrim(lower(regexp_replace(regexp_replace(df2.lastname,"['`]",""),"[-]"," ")))) \
-		.withColumn("mobilephone", regexp_extract(regexp_replace(df2.mobilephone,"\\s",""),"(\\+*(?:61)*|0*)(4\\d{8})",2)) \
-		.withColumn("state", lower(df2.state)) \
-		.withColumn("city", lower(df2.city)) \
-		.withColumn("countryname", lower(df2.countryname)) \
-		.withColumn("gender_", get_genderUDF(col("salutation"), col("firstname") + lit(" ") + col("lastname"), col("emailaddress"))) \
-		.select("customerid", "salutation", "gender_", "firstname", "lastname", "dateofbirth", 
-					"createddate", "modifieddate", "emailaddress","university", "isstudentorstaff", "state", "city","postcode",
-							"countryname","mobilephone", "homephone", "workphone") \
+		.withColumn("DateOfBirth", when(year(df2.DateOfBirth) < 1918, lit(None)).otherwise(df2.DateOfBirth)) \
+		.withColumn("FirstName", ltrim(lower(regexp_replace(df2.FirstName, "[-]"," ")))) \
+		.withColumn("LastName", ltrim(lower(regexp_replace(regexp_replace(df2.LastName,"['`]",""),"[-]"," ")))) \
+		.withColumn("MobilePhone", regexp_extract(regexp_replace(df2.MobilePhone,"\\s",""),"(\\+*(?:61)*|0*)(4\\d{8})",2)) \
+		.withColumn("State", lower(df2.State)) \
+		.withColumn("City", lower(df2.City)) \
+		.withColumn("CountryName", lower(df2.CountryName))
+
+df4 = df3.withColumn("Gender", getGenderNameUDF(df3.FirstName)) \
+		.select("CustomerID", "Salutation", "Gender", "FirstName", "LastName", "DateOfBirth", 
+					"CreatedDate", "ModifiedDate", "EmailAddress","University", "isStudentOrStaff", "State", "City","Postcode",
+							"CountryName","MobilePhone", "HomePhone", "WorkPhone") \
 		.repartition(1) \
 		.write.option("header","true").mode("overwrite").option("compression", "gzip").csv("out")
